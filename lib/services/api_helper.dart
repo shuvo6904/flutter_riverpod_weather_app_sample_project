1720c04b8cefb071ca61121af45c8669b032e6e1
shuvo <shuvo6904@gmail.com>
2025-03-20 16:06:28 +0600

    # add package

---
+++ lib/services/api_helper.dart
@@ -0,0 +1,70 @@
+
+import 'package:dio/dio.dart';
+import 'package:weather_app_tutorial/constants/constants.dart';
+import 'package:weather_app_tutorial/models/hourly_weather.dart';
+import 'package:weather_app_tutorial/models/weather.dart';
+import 'package:weather_app_tutorial/models/weekly_weather.dart';
+import 'package:weather_app_tutorial/services/geolocator.dart';
+import 'package:weather_app_tutorial/utils/logging.dart';
+
+class ApiHelper {
+  static const baseUrl = 'https://api.openweathermap.org/data/3.0';
+  static const weeklyWeatherUrl = '';
+
+  static double lat = 0.0;
+  static double lon = 0.0;
+  static final dio = Dio();
+
+  static Future<void> fetchLocation() async {
+    final location = await getLocation();
+    lat = location.latitude;
+    lon = location.longitude;
+  }
+
+  // Current Weather
+  static Future<Weather> getCurrentWeather() async {
+    await fetchLocation();
+    final url = _constructWeatherUrl();
+    final response = await _fetchData(url);
+    return Weather.fromJson(response);
+  }
+
+  // Hourly Weather
+  static Future<HourlyWeather> getHourlyForecaset() async {
+    await fetchLocation();
+    final url = _constructForecastUrl();
+    final response = await _fetchData(url);
+    return HourlyWeather.fromJson(response);
+  }
+
+  // Weekly Weather
+  static Future<WeeklyWeather> getWeeklyForecast() async {
+    await fetchLocation();
+    final url = _constructWeeklyForecastUrl();
+    final response = await _fetchData(url);
+    return WeeklyWeather.fromJson(response);
+  }
+  static String _constructWeatherUrl() =>
+      '$baseUrl/weather?lat=$lat&lon=$lon&units=metric&appid=${Constants.apiKey}';
+
+  static String _constructForecastUrl() =>
+      '$baseUrl/forecast?lat=$lat&lon=$lon&units=metric&appid=${Constants.apiKey}';
+
+  static String _constructWeeklyForecastUrl() =>
+      '$baseUrl/weeklyWeatherUrl?latitude=$lat&longitude=$lon&appid=${Constants.apiKey}';
+
+  static Future<Map<String, dynamic>> _fetchData(String url) async {
+    try {
+      final response = await dio.get(url);
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        printWarning('Failed to load data: ${response.statusCode}');
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      printWarning('Error fetching data from $url: $e');
+      throw Exception('Error fetching data');
+    }
+  }
+}
